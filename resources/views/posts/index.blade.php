f035c172fe7daf956309b9689e61a8ff573c3eee
thaemrangel <thaemrangel@gmail.com>
2020-12-19 12:21:11 -0300

    Finishing loops

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,23 @@
 
 @section('title', 'Blog Posts')
 
-@section('content')
-    @foreach ($posts as $key => $post)
-        <div>{{ $key }}.{{ $post['title'] }}</div>
-    @endforeach
+@section('content') 
+
+    <!-- If with foreach function --> 
+    @forelse ($posts as $key => $post)
+
+        {{-- @break($key == 2) | Breaks the loop  
+         @continue($key == 1) | Continues after the key --}}
+
+         @if($loop->even)
+          <div>{{ $key }}.{{ $post['title'] }}</div>
+          @else
+          <div style="background-color: silver">
+            {{ $key }}.{{ $post['title'] }}
+          </div>
+          @endif
+        @empty
+        <div>No posts found!</div> 
+
+    @endforelse  
 @endsection
